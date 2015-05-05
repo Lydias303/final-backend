1fb100aee22c127d4c505667f34e9697ad3bd975
Lydias303 <shaferlydia@yahoo.com>
2015-05-05 09:26:15 -0600

    added archived column to todo

--- db/migrate/20150505152159_create_todos.rb
+++ db/migrate/20150505152159_create_todos.rb
@@ -2,7 +2,8 @@ class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
       t.string :title
-      t.boolean :is_completed, default: true
+      t.boolean :is_completed, default: false
+      t.boolean :archived, default: true
 
       t.timestamps null: false
     end
